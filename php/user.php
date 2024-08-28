490d32c1268a2d220e437ab629b2f582d9405f35
MyoArkar <myoarkar430@gmail.com>
2024-08-28 09:01:30 +0630

    php/user.php

--- php/user.php
+++ php/user.php
@@ -6,7 +6,7 @@
   $sql = mysqli_query($conn, "SELECT * FROM users WHERE NOT unique_id ={$outgoing_id}");
   $output = "";
 
-  if(mysqli_num_rows($sql) > 0){
+  if(mysqli_num_rows($sql) < 1){
     $output .= "No users are avaliable to chat";
   }elseif(mysqli_num_rows($sql) > 0){
     include ("data.php");
